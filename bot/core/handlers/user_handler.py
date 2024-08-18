1f849bfbfeea1efa2e5f42076e014e6b892a10eb
Daniil Markov <reedtzhere@gmail.com>
2024-08-18 22:25:24 +0300

    Update user_handler.py

--- bot/core/handlers/user_handler.py
+++ bot/core/handlers/user_handler.py
@@ -25,45 +25,50 @@ async def message_handler(message: Message, _user_controller) -> None:
         )
 
         await message.answer(
-            f"Здравствуйте, <b>{message.from_user.full_name}!</b>\n\n"
-            "Вы присоединились к приложению <b>EVENTRA</b>\n\n"
-            "Нажмите \"Открыть\" для запуска\n\n",
-            reply_markup=kb.web_app_keyboard_inline
-        )
-
-        if not message.text == 'Открыть веб-апп':
-            await message.answer(
-            text="Если у Вас будут какие-то вопросы, то можете написать в тех.поддержку",
-            reply_markup=kb.tech_keyboard,
-        )
-    except DuplicateKeyError as dke:
-        logging.info(dke)
-        await message.answer(
-            "Вы уже зарегистрированы в приложении <b>EVENTRA</b>.\n\n"
-            "Нажмите \"Открыть\" для запуска\n\n",
+            text=f"""
+Здравствуйте, <b>{message.from_user.full_name}!</b>
+Вы присоединились к приложению <b>EVENTRA</b>
+Нажмите \"Открыть\" для запуска
+            """,
             reply_markup=kb.web_app_keyboard_inline,
         )
 
-        if not message.text == 'Открыть веб-апп':
+        if not message.text == "Открыть веб-апп":
             await message.answer(
-            text="Если у Вас будут какие-то вопросы, то можете написать в тех.поддержку",
-            reply_markup=kb.tech_keyboard,
+                text="Если у Вас будут какие-то вопросы, то можете написать в тех.поддержку",
+                reply_markup=kb.tech_keyboard,
+            )
+    except DuplicateKeyError as dke:
+        logging.info(dke)
+        await message.answer(
+            text=f"""
+Вы уже зарегистрированы в приложении <b>EVENTRA</b>
+Нажмите \"Открыть\" для запуска
+            """,
+            reply_markup=kb.web_app_keyboard_inline,
         )
+
+        if not message.text == "Открыть веб-апп":
+            await message.answer(
+                text="Если у Вас будут какие-то вопросы, то можете написать в тех.поддержку",
+                reply_markup=kb.tech_keyboard,
+            )
     except Exception as e:
         logging.info(e)
         await message.answer(
             "Здравствуйте. Произошла ошибка при попытке первичной регистрации.\n\nПопробуйте позже."
         )
 
+
 @user_router.message(F.text == "Тех.поддержка")
 async def print_moderators(message, _moderator_controller: ModeratorController):
     await message.bot.send_chat_action(
-            chat_id=message.from_user.id, action=ChatAction.TYPING
-        )
-    
+        chat_id=message.from_user.id, action=ChatAction.TYPING
+    )
+
     moderators = await _moderator_controller.get_moderators()
 
-    text = ''
+    text = ""
 
     for moderator in moderators:
         text += f"""
