fab2813badfe3509b73962ac33eaa4220434c01d
Daniil Markov <reedtzhere@gmail.com>
2024-08-19 20:37:50 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ const upload = multer({
 app.use("/media", express.static("media"));
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 mongoose
   .connect(process.env.NODE_DB_URL)
