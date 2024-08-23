cca79f99c9b9d284a31936c22d30fdf2adbd7b57
Knexy04 <nikitos.markin04@gmail.com>
2024-08-23 13:03:41 +0300

    уц

--- frontend/src/components/AddArtistRequest/doneArtistRequest.jsx
+++ frontend/src/components/AddArtistRequest/doneArtistRequest.jsx
@@ -1,4 +1,4 @@
-import done from "../../images/done.png"
+import donereview from "../../images/donereview.png"
 import { LightButton2 } from "../UI/Button/button";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -16,7 +16,8 @@ const DoneArtistRequest = () => {
 
     return (
         <div className="h-screen flex flex-col items-center justify-center px-[16px]">
-            <img src={done} alt="done" className="mb-[72px]" />
+            <img src={donereview} alt="done" className="mb-[8px]" />
+            <div className="text-center font-bold text-[24px] mb-[72px]">Ваша анкета на модерации</div>
             <div className="w-full"><LightButton2 onClick={handleGoArtistCatalog} text={"Каталог заявок"} /></div>
             <Link to={"/catalog-artist"} className="mt-[12px] w-full underline text-center">Анкеты других артистов</Link>
         </div>
